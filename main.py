0a2f1824e0ae42e625d844b088e29d285f81e564
Marcello Russo <marcellorusso.x@gmail.com>
2023-01-07 18:14:00 +0100

    modifica dell'ordine di esecuzione dei comandi

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import subprocess
 cron = croniter.croniter('*/5 * * * *', time.time())
 
 while True:
+    # Eseguo il file fillTrafficRoutes.py usando subprocess
+    subprocess.run(["python", "fillTrafficRoutes.py"])
+
     # Ottiengo il prossimo momento in cui il cron job deve essere eseguito
     next_execution = cron.get_next()
 
@@ -14,6 +17,3 @@ while True:
 
     # Dormo per il tempo necessario
     time.sleep(sleep_time)
-
-    # Eseguo il file fillTrafficRoutes.py usando subprocess
-    subprocess.run(["python", "fillTrafficRoutes.py"])
